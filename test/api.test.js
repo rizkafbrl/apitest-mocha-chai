a7e3f48bfbc3e7dbfa1b774e5b576096e820540d
Rizka Febrila Sari <rizka.febrila@efishery.com>
2025-02-06 22:05:19 +0700

    Initial commit for Node.js Mocha and Chai project

---
+++ test/api.test.js
@@ -0,0 +1,50 @@
+// test/api.test.js
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('endpoint reqres in', () => {
+  it('should return a total of 12 users', (done) => {
+    chai.request('https://reqres.in')
+      .get('/api/users?page=2')
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        // Verify that the response has status 200
+        expect(res).to.have.status(200);
+
+        // Verify the response is an object
+        expect(res.body).to.be.an('object');
+
+        // Verify that the total number of users is 12
+        expect(res.body.total).to.equal(12);
+
+        // Optionally, you can add more assertions here
+        done();
+      });
+  });
+
+  it('get single user id=2', (done) => {
+    chai.request('https://reqres.in')
+      .get('/api/users/2')
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        // Verify that the response has status 200
+        expect(res).to.have.status(200);
+
+        // Verify the response is an object
+        expect(res.body).to.be.an('object');
+        console.log(res.body);
+        expect(res.body.data.id).to.equal(2);
+    
+        done();
+      });
+  });
+
+});
---
+++ test/sample.test.js
@@ -0,0 +1,10 @@
+// test/sample.test.js
+
+const { expect } = require('chai');
+
+describe('Sample Test', () => {
+  it('should return true when comparing true', () => {
+    expect(true).to.be.true;
+  });
+});
+
